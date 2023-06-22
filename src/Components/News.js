1484daa8e37bdcf6bd50855e4b9f47f9379a8732
Jamal Ahmad <qja786@gmail.com>
2023-06-22 22:35:53 +0530

    22 june night

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,38 +7,62 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      DataisLoaded: false
+      DataisLoaded: false,
+      page:1
     }
   }
   componentDidMount() {
-    fetch(  
-"https://newsapi.org/v2/top-headlines?country=in&pageSize=4&apiKey=cb475762b09d446bb335985c3453faca")
+    fetch( 
+`https://newsapi.org/v2/top-headlines?country=in&pageSize=4&apiKey=cb475762b09d446bb335985c3453faca&page=${this.page}`)
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+              articles: json.articles
+            });
+            
+        })
+}
+
+handlePrevClick = async () => {
+  
+   fetch(
+`https://newsapi.org/v2/top-headlines?country=in&pageSize=4&apiKey=cb475762b09d446bb335985c3453faca&page=${this.state.page -1}`)
         .then((res) => res.json())
         .then((json) => {
             this.setState({
               articles: json.articles,
-              DataisLoaded: true
+              page:this.state.page - 1
             });
         })
 }
+handleNextClick = async () => {
+   fetch( 
+`https://newsapi.org/v2/top-headlines?country=in&pageSize=4&apiKey=cb475762b09d446bb335985c3453faca&page=${this.state.page +1}`)
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+              articles: json.articles,
+              page:this.state.page + 1
+            });
+        })
+}
+
   render() {
     
-    const { articles } = this.state;
-    console.log(articles);
+    const {articles} = this.state;
     return (
       <div className='container my-3'>
         <h2>DailyNews - Top Headlines</h2>
-        <div className="row">
+        <div className="row" >
           { articles.map((article) => {
-            console.log(article);
               return <div className="col-md-4" key={article.url}>
               <NewsItem imageUrl={article.urlToImage} title={article.title} description={article.description} newsUrl={article.url}/>
             </div>
           })}
         </div>
         <div className='d-flex justify-content-between'>
-          <button  type="button" class="btn btn-dark"> &larr; Previous</button>
-          <button type="button" class="btn btn-dark">Next &rarr;</button>
+          <button disabled={this.state.page <=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
         
       </div>
